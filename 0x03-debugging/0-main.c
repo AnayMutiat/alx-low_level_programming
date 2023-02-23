614c36f0b1a4239fe0ba293e39a55d1c842e5d73
AnayMutiat <mutiatanay@gmail.com>
2023-02-23 13:12:41 -0800

    O task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * main - Test function for positive or negative
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
