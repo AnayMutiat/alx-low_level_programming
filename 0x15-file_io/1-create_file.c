2e5496bc20aa13356cb6255ad03b876dc6920ebf
Anay Mutiat <mutiatanay@gmail.com>
2023-08-22 09:29:36 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,7 @@
  * Return: if the funtion fails - -1.
  * Otherwise - 1.
  */
-
-int create_file (const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
@@ -22,7 +21,7 @@ int create_file (const char *filename, size_t letters)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT |O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
